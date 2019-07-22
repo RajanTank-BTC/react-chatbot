d9d826986004953adf56a920b4a4dacc9f78ed41
harishchobisa <harish.chobisa@botreetechnologies.com>
2019-07-22 18:49:55 +0530

    UI upadtes

--- src/component/chatbot.js
+++ src/component/chatbot.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ChatBot from 'react-simple-chatbot'
+import { ThemeProvider } from 'styled-components';
+import chatIcon from '../chat.svg';
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'arial',
+  headerBgColor: '#0CD7FF',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#E5F9FC',
+  botFontColor: '#000',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
@@ -13,16 +27,14 @@ class ApiComponent extends React.Component {
 
   }
 
-  handleClick = (id) => {
+  handleClick = (name) => {
 
     const { triggerNextStep, nextTrigger } = this.props
-    console.log(id, nextTrigger)
-    console.log(this.props, "dd")
     triggerNextStep({
-      value: id,
+      value: name,
       customOption: {
-        value: `${id}`,
-        label: `${id}`,
+        value: `${name}`,
+        label: `${name}`,
         trigger: nextTrigger
       },
       trigger: nextTrigger
@@ -42,24 +54,20 @@ class ApiComponent extends React.Component {
 
   render() {
     return (
-      <div >
-        <div>
-          <p style={{ fontSize: 20, color: 'black' }}>Search product</p>
-          <input type='text' name='search' value={this.state.value} onChange={event => { this.setState({ value: event.target.value }) }}
+      <div className="search-box">
+        <h3>Search product</h3>
+        <input type='text' name='search' value={this.state.value} onChange={event => { this.setState({ value: event.target.value }) }}
             onKeyPress={this.handlePress} />
-        </div>
-        <table>
-          <tbody>
-            {this.state.posts &&
-              this.state.posts.map((post) => {
-                return (<tr key={post.id}>
-                  <td style={{ color: 'black', fontSize: 20 }}>Title</td>
-                  <td style={{ color: 'black', fontSize: 20, cursor: 'pointer' }}
-                    onClick={() => this.handleClick(post.id)}>{post.id}</td>
-                </tr>)
-              })}
-          </tbody>
-        </table>
+        {this.state.posts.length > 0  && 
+        <ul>
+          {this.state.posts &&
+            this.state.posts.map((post) => {
+              return (<li key={post.id} onClick={() => this.handleClick(post.title)}>
+                <title>Title</title>
+                <p>{post.id}</p>
+              </li>)
+            })}
+        </ul>}
       </div>
     )
   }
@@ -77,8 +85,6 @@ class ChatbotDemo extends React.Component {
   }
 
   apiCall = ({ steps, value }) => {
-    console.log(steps)
-    console.log(value)
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(json => this.setState({ posts: json }))
@@ -87,78 +93,81 @@ class ChatbotDemo extends React.Component {
   render() {
     const { opened } = this.state;
     return (
-      <div style={{ alignItems: 'right' }}>
-        <ChatBot
-          floating={true}
-          opened={opened}
-          toggleFloating={this.toggleFloating}
-          handleEnd={this.apiCall}
-          // style={{ width: '100%' }}
-          steps={[
-            {
-              id: '1',
-              message: 'please let me know which product you want to check.',
-              trigger: '2',
-            },
-            {
-              id: '2',
-              component: <ApiComponent nextTrigger='3' />,
-            },
-            {
-              id: '3',
-              message: 'ok can you please specify how much quantity you want?',
-              trigger: 4
-            },
-            {
-              id: '4',
-              placeholder: 'Type how much quantity ...',
-              user: true,
-              trigger: '5'
-            },
-            {
-              id: '5',
-              message: 'ok can I have your email address so that we can send the quotation?',
-              trigger: '6'
-            },
-            {
-              id: '6',
-              user: true,
-              placeholder: 'Type your email ...',
-              validator: (value) => {
-                if (!emailRegex.test(value)) {
-                  return 'Please Enter valid email'
-                }
-                return true
+      <ThemeProvider theme={theme}>
+        <div style={{ alignItems: 'right' }}>
+          <ChatBot
+            floating={true}
+            opened={opened}
+            toggleFloating={this.toggleFloating}
+            handleEnd={this.apiCall}
+            floatingIcon= {chatIcon}
+            className= "chat-box"
+            steps={[
+              {
+                id: '1',
+                message: 'please let me know which product you want to check.',
+                trigger: '2',
               },
-              trigger: '7'
-            },
-            {
-              id: '7',
-              message: 'thank you very much, we shall get back to you over email.',
-              end: true
-            }
-          ]}
-        />
-        <div>
-          <table>
-            <td>Title</td>
-            <td>Id</td>
-            <td>Message</td>
-            <tbody>
-              {this.state.posts.map((post) => {
-                return (
-                  <tr>
-                    <td>{post.userId}</td>
-                    <td>{post.title}</td>
-                    <td>{post.id}</td>
-                  </tr>
+              {
+                id: '2',
+                component: <ApiComponent nextTrigger='3' />,
+              },
+              {
+                id: '3',
+                message: 'ok can you please specify how much quantity you want?',
+                trigger: 4
+              },
+              {
+                id: '4',
+                placeholder: 'Type how much quantity ...',
+                user: true,
+                trigger: '5'
+              },
+              {
+                id: '5',
+                message: 'ok can I have your email address so that we can send the quotation?',
+                trigger: '6'
+              },
+              {
+                id: '6',
+                user: true,
+                placeholder: 'Type your email ...',
+                validator: (value) => {
+                  if (!emailRegex.test(value)) {
+                    return 'Please Enter valid email'
+                  }
+                  return true
+                },
+                trigger: '7'
+              },
+              {
+                id: '7',
+                message: 'thank you very much, we shall get back to you over email.',
+                end: true
+              }
+            ]}
+          />
+          <div>
+            <table>
+              <td>Title</td>
+              <td>Id</td>
+              <td>Message</td>
+              <tbody>
+                {this.state.posts.map((post) => {
+                  return (
+                    <tr>
+                      <td>{post.userId}</td>
+                      <td>{post.title}</td>
+                      <td>{post.id}</td>
+                    </tr>
 
-                )
-              })}
-            </tbody>
-          </table>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     )
   }
 }
